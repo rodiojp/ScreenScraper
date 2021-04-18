4f9b32e21fe3ef5f353d409c7017083870462504
yurirod <yurirobins@gmail.com>
2021-04-18 16:22:45 -0400

    Add project files.

---
+++ ScreenScraper.Domain/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenScraper.Domain
+{
+    public class Class1
+    {
+    }
+}
